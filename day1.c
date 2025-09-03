c6a493d6e95f471dcae9334ed0b6dbe3b8953fab
Singireddy Geetha Sri Vara Lakshmi <161279800+geethasri22@users.noreply.github.com>
2025-09-03 14:47:55 +0530

    day1.c

---
+++ day1.c
@@ -0,0 +1,36 @@
+//1) while loop:
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    while(i<=10)
+    {
+        printf("\nyes , %d",i);
+        i++;
+    }
+}
+
+//2)for loop:
+ #include <stdio.h>
+
+int main() {
+    for(int k=200;k<=400;++k){
+        printf("\n %d",k);
+    }
+}
+
+//3)Do-While:
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    do{
+     printf("\n PPT");
+     i++;
+     
+    }while(i<=10);
+   
+}
+1. **While Loop:** A basic example iterating from 1 to 10.
+2. **For Loop:** An example demonstrating iteration from 200 to 400.
+3. **Do-While Loop:** A loop that prints "PPT" ten times.
