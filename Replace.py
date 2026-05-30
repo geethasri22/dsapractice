78f83d14bb8347794a8c9da8fc0b2ff64c5ccfaa
Singireddy Geetha Sri Vara Lakshmi <singireddygeethasri@gmail.com>
2026-05-30 20:39:09 +0530

    Implement replaceElements method in Solution class

---
+++ Replace.py
@@ -0,0 +1,12 @@
+class Solution:
+    def replaceElements(self, arr):
+        # code here
+        n = len(arr)
+        if n <= 1:
+            return arr
+        temp = list(arr)
+        arr[0]= temp[0] ^ temp[1]
+        for i in range(1,n-1):
+            arr[i]=temp[i-1] ^ temp[i+1]
+        arr[n-1] = temp[n-2] ^ temp[n-1]
+        return arr
